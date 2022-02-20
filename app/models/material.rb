b619d81cc6c19e9baab68f591bad69cd77c52d44
Vinicius Libero <libero.moreira@gmail.com>
2022-02-20 15:31:37 -0300

    edited material and quantity models

--- app/models/material.rb
+++ app/models/material.rb
@@ -2,18 +2,16 @@ class Material < ApplicationRecord
   has_many :quantities
 
   validates :name, uniqueness: true
+  validate :check_material_log, on: :destroy
 
-  before_destroy :check_material_log
-
+  after_create :create_quantity_zero
 
   def check_material_log
     erros.add(:name, "Material nao pode ser excluido pois estah incluido no registro") if Log.find_by(material_name: self.name).present?
   end
 
-  def check_time
-
+  def create_quantity_zero
+    Quantity.create(material_id: self.id, amount: 0)
   end
-
-
 
 end
--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -2,18 +2,29 @@ class Quantity < ApplicationRecord
   belongs_to :material, foreign_key: :material_id, class_name: 'Material'
 
   validate :check_negative_balance
+  validate :check_working_time
 
   after_save :create_material_log
 
   def create_material_log
-    Log.create(
-      user_email: ApplicationController.logged_user_email,
-      material_name: self.material.name,
-      quantity_delta: self.amount
-    )
+    unless self.amount == 0 && self == self.material.quantities.order(id: :asc).first
+      Log.create(
+        user_email: ApplicationController.logged_user_email,
+        material_name: self.material.name,
+        quantity_delta: self.amount
+      )
+    end
   end
 
-    def check_negative_balance
-      errors.add(:amount, "Quantidade total nao pode ser negativa") if self.material.quantities.pluck(:amount).sum < 0
+  def check_negative_balance
+    errors.add(:amount, "Quantidade total nao pode ser negativa") if self.material.quantities.pluck(:amount).sum < 0
+  end
+
+  def check_working_time
+    if self.amount < 0
+      if (Date.today.saturday? || Date.today.sunday?) || (Time.now < Time.parse("09:00") || Time.now > Time.parse("18:00"))
+        errors.add(:amount, "Retirada de materiais so pode ser feita entre 9h e 18h de segunda a sexta")
+      end
     end
+  end
 end
