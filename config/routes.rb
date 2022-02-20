c6a3edc79ed3e0411741dfa6f083fad279189bb4
Vinicius Libero <libero.moreira@gmail.com>
2022-02-20 19:14:14 -0300

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :quantities, only: [:create]
   resources :materials
   devise_for :users, except: [:destroy]
-  root to: 'materials#index'
+  root to: 'pages#home'
+  get 'logs', to: 'pages#logs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
