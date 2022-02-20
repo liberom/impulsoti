737de99f0d90e0a1ac47c4191492a66ea041137a
Vinicius Libero <libero.moreira@gmail.com>
2022-02-19 23:07:05 -0300

    added methods to material model and included method in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def logged_user_email
+    if user_signed_in?
+      session[:user_email] = current_user.email
+      session[:user_email]
+    end
+  end
+
 end
--- app/models/material.rb
+++ app/models/material.rb
@@ -1,2 +1,21 @@
 class Material < ApplicationRecord
+  validates :name, uniqueness: true
+
+  after_save :create_material_log
+  before_destroy :check_material_log
+
+  def create_material_log
+    material = Material.find_by(name: self.name)
+    Log.create(
+      user_email: ApplicationController.logged_user_email,
+      material_name: self.name,
+      quantity_delta: (material.quantity == 0 ? self.quantity : (material.quantity - self.quantity))
+    )
+  end
+
+  def check_material_log
+    erros.add(:name, "Material nao pode ser excluido pois estah incluido no registro") if Log.find_by(material_name: self.name).present?
+  end
+
+
 end
