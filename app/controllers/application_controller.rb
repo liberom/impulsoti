ec4c3a568653680057163130d3cdf139ac7ec5c6
Vinicius Libero <libero.moreira@gmail.com>
2022-02-20 19:34:56 -0300

    removed method from app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def logged_user_email
-    if user_signed_in?
-      session[:user_email] = current_user.email
-      session[:user_email]
-    end
-  end
-
   def after_sign_in_path_for(resource)
     materials_path
   end
